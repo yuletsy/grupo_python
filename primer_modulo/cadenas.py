22e206fa5fb5f60a4de142d2580d8daa5c72b7d3
yuletsypabon <yuletsyp.pabonf@unac.edu.co>
2021-03-23 12:38:11 -0500

    cadenas

---
+++ primer_modulo/cadenas.py
@@ -0,0 +1,30 @@
+# principales funciones y características de las cadenas de texto
+
+"""
+python trata a las cadenas como "listas/arrays" de caracteres
+ej:
+    nombre = "luis alvarez"
+    esto equivale a: l , u , i , s ,   , a , l , v , a , r , e , z
+    posiciones:      0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11
+se puede acceder a cualquier posicion de la candena tal que...
+    nombre[<posicion deseada>] (cabe resaltar que la posicion debe estar
+                                contemplada en la cadena)
+también es valido acceder a una porción de la cadena tal que...
+    nombre[<pos inico>:<pos final>]
+concatenación = "cadena1" + "cadena2"
+(no se puede modificar mediante la consulta)
+    nombre[i] = "m"
+Métodos: 
+    metodo len(<cadena/variable>) devuelve el tamaño de la cadena
+    metodo upper()/lower() convierte la cadena en mayusculas/minusculas
+    metodo split(<argumento>) separa la cadena dependiendo del argumento y la almacena en un array
+        onde cada pos es una parte de la cadena
+"""
+
+
+def main():
+
+    nombre = "luis alvarez"
+
+
+main()
