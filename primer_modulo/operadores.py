b2f03ef6d1b1b75f38ee859dcdc1f30a1be6235b
yuletsypabon <yuletsyp.pabonf@unac.edu.co>
2021-03-23 12:36:00 -0500

    operadores

---
+++ primer_modulo/operadores.py
@@ -0,0 +1,21 @@
+"""
+aritmeticos :
+    suma, resta, multiplicacion, division, exponencial, residuo, division entera
+comparacion (return bool):
+    igual que, mayor que, menor que, diferente de, mayor o igual, menor o igual
+logicos :
+    si (and), o (or), negación (not)
+asignacion :
+    igual, incremento, decremento, multiplicado, dividido, modulo, div. entera, exponente
+especiales
+    esta (is), no está (is not), en (in), no en (not in)
+"""
+
+
+def main():
+
+    num1 = 2
+    num2 = 4
+
+
+main()
