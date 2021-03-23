e88f04f76591fcc0154e985a42807de044a5664f
yuletsypabon <yuletsyp.pabonf@unac.edu.co>
2021-03-23 12:34:38 -0500

    tipos de datos

---
+++ primer_modulo/variables.py
@@ -0,0 +1,35 @@
+"""
+# tipos de datos:
+numéricos :
+    integer : enteros (no decimal)
+    float : decimales
+cadenas :
+    char : un solo caracter = 'a', '6', '?'
+    string : texto de más de un caracter = "esto es un texto de más de un caracter"
+booleanos :
+    True : verdadero
+    False : falso
+"""
+
+
+def main():
+
+    integer = 3
+    variable_float = 3.3
+
+    cadena = "cadena de ejemplo"
+    caracter = 'c'
+
+    bool_verdadero = True
+    bool_falso = False
+
+    print(integer)
+    print(type(integer))
+    # print(variable_float)
+    # print(cadena)
+    # print(caracter)
+    # print(bool_verdadero)
+    # print(bool_falso)
+
+
+main()
