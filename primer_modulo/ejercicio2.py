0c4ddf2156d10b282045a49c3e5d7cfa429ac8c0
yuletsypabon <yuletsyp.pabonf@unac.edu.co>
2021-04-27 19:28:27 -0500

    ejercicio operaciones basicas

--- primer_modulo/ejercicio2.py
+++ primer_modulo/ejercicio2.py
@@ -1,71 +1,31 @@
-'''
-crear un programa que le pida al usuario su edad, nombre, dos números y muestre por pantalla:
-    - suma
-    - resta
-    - multiplicación
-    - o división (dependiendo de la opcion)
-    - de ambos números seguidos de su nombre y edad
-    NOTA: si el usuario no es mayor de 5 años, no puede usar el programa
-
-    ingrese su nombre
-    ingrese su edad
-    ingrese el primer numero 
-    ingrese el segundo numero 
-
-    1. suma
-    2. resta
-    3. multi
-    4. division
-    resultado de la operación : X 
-    Nombre: 
-    Edad: 
-    
-'''
-
 def main():
 
     nombre = str(input("Ingrese su nombre: "))
     edad = int(input("Ingrese su edad: "))
 
     if (edad > 5):
-        print("bienvenido al programa")
+        print("Bienvenido al programa de operaciones basicas")
         
         num1 = int(input("\n Ingrese el 1er numero: "))
         num2 = int(input("Ingrese el 2do numero: "))
 
         opcion = (str(input("Elija una opcion siendo 1 suma, 2 resta, 3 multiplicacion, 4 division: ")))
     
-        op_suma = "1"
-        op_resta = "2"
-        op_multi = "3"
-        op_divi = "4"
-
-        if(opcion == op_suma):
+        if(opcion ==  "1"):
             print(f"El resultado de la suma es: {(num1 + num2)}")
-        elif(opcion == op_resta):
+        elif(opcion ==  "2"):
             print(f"El resultado de la resta es: {(num1 - num2)}")
-        elif(opcion == op_multi):
+        elif(opcion ==  "3"):
             print(f"El resultado de la multiplicacion es: {(num1 * num2)}")
-        elif(opcion == op_divi):
+        elif(opcion ==  "4"):
             print(f"El resultado de la division es: {(num1 / num2)}")
         else:
             print("ingrese otra funcion\n")
 
-        print(f"nombre: {(nombre)}")
-        print(f"edad: {(edad)}")
-
+        print(f"Tu nombre es: {(nombre)}")
+        print(f"Tienes: {(edad)} años")
     else:
-        print("No puede usar el programa")
-    
-   
-
-
-
-
-
-
-
-
+        print("No puede usar el programa porque es menor de 5 años")
 main()
 
 
